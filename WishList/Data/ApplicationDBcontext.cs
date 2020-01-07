abc4c5213684b775097ab5fbaa4d1612160c2e90
kaleessw <rajendran.kaleesswaramoorthi@cibc.com>
2020-01-07 16:25:40 -0500

    2

--- WishList/Data/ApplicationDBcontext.cs
+++ WishList/Data/ApplicationDBcontext.cs
@@ -7,10 +7,10 @@ using WishList.Models;
 
 namespace WishList.Data
 {
-    public class ApplicationDBcontext : DbContext
+    public class ApplicationDbContext : DbContext
     {
 		public	DbSet<Item> Items { get; set; }
-		public	ApplicationDBcontext(DbContextOptions options) : base(options)
+		public	ApplicationDbContext(DbContextOptions options) : base(options)
         {
 
 
