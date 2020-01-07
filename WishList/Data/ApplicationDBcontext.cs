cf462cc1004c8df3e2c0b2aa9e1a69cb1b79a6a6
kaleessw <rajendran.kaleesswaramoorthi@cibc.com>
2020-01-07 14:55:34 -0500

    items

---
+++ WishList/Data/ApplicationDBcontext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDBcontext : DbContext
+    {
+		public	DbSet<Item> Items { get; set; }
+		public	ApplicationDBcontext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+    }
+}
--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-
+using WishList.Data;
 namespace WishList
 {
     public class Startup
@@ -17,6 +17,7 @@ namespace WishList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<ApplicationDBcontext>(options => options.UseInMemoryDatabase("WishList"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
